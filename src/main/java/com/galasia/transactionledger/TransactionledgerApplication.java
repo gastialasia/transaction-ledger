6e10a45e30d9d4d84de1862637bfb7c61e29b658
Gaston Alasia <86937493+gastialasia@users.noreply.github.com>
2026-06-13 14:49:40 -0300

    Initial Spring Boot project setup

---
+++ src/main/java/com/galasia/transactionledger/TransactionledgerApplication.java
@@ -0,0 +1,13 @@
+package com.galasia.transactionledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionledgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionledgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galasia/transactionledger/TransactionledgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galasia.transactionledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionledgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
