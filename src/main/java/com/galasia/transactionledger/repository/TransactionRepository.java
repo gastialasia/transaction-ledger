c3a1288f54c6317e45e432569661eb3fbafd2f64
Gaston Alasia <86937493+gastialasia@users.noreply.github.com>
2026-06-13 15:52:53 -0300

    refactor: optimized findIdsByType by adding transactionIdsByType map

--- src/main/java/com/galasia/transactionledger/repository/TransactionRepository.java
+++ src/main/java/com/galasia/transactionledger/repository/TransactionRepository.java
@@ -3,24 +3,31 @@ package com.galasia.transactionledger.repository;
 import com.galasia.transactionledger.model.Transaction;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 
 @Repository
 public class TransactionRepository {
 
     private final Map<Long, Transaction> transactions = new ConcurrentHashMap<>();
+    private final Map<String, List<Long>> transactionIdsByType = new ConcurrentHashMap<>();
 
     public void save(Transaction transaction) {
         transactions.put(transaction.getId(), transaction);
+        transactionIdsByType
+                .computeIfAbsent(transaction.getType(), k -> new CopyOnWriteArrayList<>())
+                .add(transaction.getId());
     }
 
     public List<Long> findIdsByType(String type) {
-        return transactions.values().stream()
-                .filter(t -> t.getType().equals(type))
-                .map(Transaction::getId)
-                .toList();
+        return Collections.unmodifiableList(
+                transactionIdsByType.getOrDefault(type, List.of())
+        );
     }
 }
+
+
